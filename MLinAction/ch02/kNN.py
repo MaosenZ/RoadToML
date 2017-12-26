738d155cbf628df357201fd1ed93ac27cf82bf56
mason <mason@mason-mbp.attlocal.net>
2017-12-25 21:05:47 -0800

    added handwritingrecognition example

--- MLinAction/ch02/kNN.py
+++ MLinAction/ch02/kNN.py
@@ -1,5 +1,6 @@
 from numpy import *
 import operator
+from os import listdir
 
 def createDataSet():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -10,9 +11,9 @@ def createDataSet():
 def classify0(inX, dataSet, labels, k):
     #calculate distances
     dataSetSize = dataSet.shape[0] #number of samples (elements)
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet #sum of each element
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
     sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)
+    sqDistances = sqDiffMat.sum(axis=1) #sum of each element
     distances = sqDistances**0.5
     sortedDistIndicies = distances.argsort()
     #voting with lowest k distances
@@ -76,3 +77,37 @@ def classifyPerson():
     inArr = array([ffMiles, percentTats, iceCream])
     classifierResult = classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
     print "You will probably like this person: ", resultList[classifierResult-1]
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print "\nthe total number of errors is: %d" % errorCount
+    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
